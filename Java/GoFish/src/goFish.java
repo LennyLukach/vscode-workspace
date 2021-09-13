fdb9abce60ec8132717229689daa3bc26907aa2d
lbxmega <80704991+lbxmega@users.noreply.github.com>
2021-09-13 14:47:45 -0700

    kewl

--- Java/GoFish/src/goFish.java
+++ Java/GoFish/src/goFish.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 import java.util.Collections;
 import java.util.ArrayList;
-
+// TODO: Make books
+// TODO: Make so players can only ask for cards they have
 public class goFish {
 
     public static void main(String[] args) {
@@ -18,13 +19,11 @@ public class goFish {
         dealerHand.displayHand();
         System.out.println("\n");
 
+        System.out.println("Which card do you want to ask for?\n");
         String askFor = in.nextLine();
         for (int x = 0; x < dealerHand.size(); x++) {
-            System.out.println(askFor + " " + dealerHand.getCard(x).idName);
-            System.out.println(dealerHand.getCard(x).idName.equals(askFor));
             if (dealerHand.getCard(x).idName.equals(askFor)) {
-                System.out.println("should work!");
-                Card theCard = new Card(dealerHand.getCard(x).value, dealerHand.getCard(x).suit, dealerHand.getCard(x).name, dealerHand.getCard(x).idName);
+                Card theCard = new Card(dealerHand.getCard(x).value, dealerHand.getCard(x).idName);
                 x = dealerHand.removeCard(theCard, userHand, x);
             }
         }
@@ -41,7 +40,7 @@ public class goFish {
 
 class Deck {
     ArrayList<Card> deck;
-    String suits[] = {"spades", "hearts", "diamonds", "clubs"};
+    String nameLists[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
     String idName;
     Deck() {
 
@@ -50,23 +49,23 @@ class Deck {
 			for (int count = 2; count < 15; count++) {
 				if (count == 11) {
                     idName = "jack";
-					deck.add(new Card(10, suits[amt], "Jack of " + suits[amt], idName));
+					deck.add(new Card(11, idName));
 				}
 				else if (count == 12) {
                     idName = "queen";
-					deck.add(new Card(10, suits[amt], "Queen of " + suits[amt], idName));
+					deck.add(new Card(12, idName));
 				}
 				else if (count == 13) {
                     idName = "king";
-					deck.add(new Card(10, suits[amt], "King of " + suits[amt], idName));
+					deck.add(new Card(13, idName));
 				}
 				else if (count == 14) {
                     idName = "ace";
-					deck.add(new Card(11, suits[amt], "Ace of " + suits[amt], idName));
+					deck.add(new Card(14, idName));
 				}
 				else {
-                    idName = Integer.toString(count);
-					deck.add(new Card(count, suits[amt], count + " of " + suits[amt], idName));
+                    idName = nameLists[count - 1];
+					deck.add(new Card(count, idName));
 				}
 			}
 		}
@@ -103,9 +102,21 @@ class Hand {
     }
 
     public void displayHand() {
+        
+        int n = this.hand.size();
+        for (int i = 0; i < n-1; i++) {   
+            for (int j = 0; j < n-i-1; j++) {
+                if (this.hand.get(j).value > this.hand.get(j + 1).value) {
+                    Card temp = this.hand.get(j);
+                    Card temp2 = this.hand.get(j + 1);
+                    this.hand.set(j, temp2);
+                    this.hand.set(j + 1, temp);
+                }
+            }
+        }
         for (int x = 0; x < this.hand.size(); x++) {
-			System.out.print(this.hand.get(x).name + " | ");
-		}
+            System.out.print(this.hand.get(x).idName + " | ");
+        }
     }
 
     public int removeCard(Card card, Hand turnPerson, int x) {
@@ -115,7 +126,7 @@ class Hand {
     }
 
     public void addCard(Card card) {
-        this.hand.add(new Card(card.value, card.suit, card.name, card.idName));
+        this.hand.add(new Card(card.value, card.idName));
     }
 
     public int size() {
@@ -125,17 +136,17 @@ class Hand {
     public Card getCard(int x) {
         return this.hand.get(x);
     }
+
+    public void makeBook() {
+        
+    }
 }
 
 class Card {
     int value;
-    String suit;
-    String name;
     String idName;
-    Card(int value, String suit, String name, String idName) {
+    Card(int value, String idName) {
         this.value = value;
-        this.suit = suit;
-        this.name = name;
         this.idName = idName;
     }
 
