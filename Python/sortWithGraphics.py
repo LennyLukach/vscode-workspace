5686a4577e3e051ad8d9e800d61f8bbf505bd564
lbxmega <80704991+lbxmega@users.noreply.github.com>
2021-10-01 10:08:19 -0700

    adf

---
+++ Python/sortWithGraphics.py
@@ -0,0 +1,13 @@
+import random
+from usefulCustomImports import *
+import time
+
+clearScr()
+
+lineLengths = [7, 3, 4, 1, 6]
+
+for length in lineLengths:
+    print("-" * length)
+
+for length in lineLengths:
+    pass
