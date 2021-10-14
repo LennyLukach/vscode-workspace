2bd0d75ab084094a6a5e67faf442b9a2e53af316
lbxmega <drlendog@gmail.com>
2021-10-13 20:27:43 -0700

    donezo

--- Python/betterDrawing.py
+++ Python/betterDrawing.py
@@ -12,7 +12,7 @@ from pygame.sprite import collide_circle
 pygame.init()
 
 SCREEN = pygame.display.set_mode((1280, 800))
-pygame.display.set_caption("Drawing Shapes")
+pygame.display.set_caption("Drawing Simulator")
 
 #! COLORS
 RED = (255, 0, 0)
@@ -23,31 +23,46 @@ BLUE = (0, 0, 255)
 PURPLE = (127, 0, 255)
 PINK = (255, 0, 127)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 #! Variables:
 left_click = False
 last_pos = None
 pen_color = WHITE
+pen_size = 2
+last_pen_size = pen_size
+myfont = pygame.font.SysFont('Comic Sans MS', 12)
 
 optionBook = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, PINK, WHITE]
 
 #red, orange, yellow, green, blue, indigo, violet, white, eraser(black)
 #small pen, big pen, clear screen
 
-pygame.draw.rect(SCREEN, (155, 155, 155), (0, 725, 1280, 75))
+def createGUI():
+    pygame.draw.rect(SCREEN, (155, 155, 155), (0, 725, 1280, 75))
 
-pygame.draw.circle(SCREEN, RED, (40, 762.5), 25) 
-pygame.draw.circle(SCREEN, ORANGE, (109 * 1 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, YELLOW, (109 * 2 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, GREEN, (109 * 3 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, BLUE, (109 * 4 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, PURPLE, (109 * 5 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, PINK, (109 * 6 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, WHITE, (109 * 7 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, RED, (109 * 8 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, RED, (109 * 9 + 40, 762.5), 25) 
-pygame.draw.circle(SCREEN, RED, (109 * 10 + 40, 762.5), 25)
-pygame.draw.circle(SCREEN, RED, (1240, 762.5), 25)
+    pygame.draw.circle(SCREEN, RED, (40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, ORANGE, (109 * 1 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, YELLOW, (109 * 2 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, GREEN, (109 * 3 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, BLUE, (109 * 4 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, PURPLE, (109 * 5 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, PINK, (109 * 6 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, WHITE, (109 * 7 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, BLACK, (109 * 8 + 40, 762.5), 25) 
+    pygame.draw.circle(SCREEN, BLACK, (109 * 9 + 40, 762.5), 27) 
+    pygame.draw.circle(SCREEN, BLACK, (109 * 10 + 40, 762.5), 27)
+    pygame.draw.circle(SCREEN, BLACK, (1240, 762.5), 27)
+    small_pen_text = myfont.render("Small Pen", True, (255, 255, 255))
+    large_pen_text = myfont.render("Large Pen", True, (255, 255, 255))
+    clear_text = myfont.render("Clear", True, (255, 255, 255))
+    canvas_text = myfont.render("Canvas", True, (255, 255, 255))
+    SCREEN.blit(small_pen_text, (109 * 9 + 13, 752.5))
+    SCREEN.blit(large_pen_text, (109 * 10 + 13, 752.5))
+    SCREEN.blit(clear_text, (109 * 11 + 24, 747.5))
+    SCREEN.blit(canvas_text, (109 * 11 + 24, 760.5))
+
+createGUI()
 
 while True:
     for event in pygame.event.get():
@@ -59,8 +74,26 @@ while True:
             if event.button == 1:
                 left_click = True
                 if math.sqrt(((mouse_pos_x % 109 - 40)**2) + ((mouse_pos_y - 762.5)**2)) < 25:
-                    pen_color = SCREEN.get_at(pygame.mouse.get_pos())
-
+                    if mouse_pos_x < 961:
+                        pen_color = SCREEN.get_at(pygame.mouse.get_pos())
+                        if pen_size != BLACK:
+                            pen_size = last_pen_size
+                        if pen_color == BLACK:
+                            last_pen_size = pen_size
+                            pen_size = 14
+                    elif mouse_pos_x > 961  and mouse_pos_x < 1075:
+                        if math.sqrt(((mouse_pos_x % 109 - 40)**2) + ((mouse_pos_y - 762.5)**2)) < 27:
+                            pen_size = 2
+                            last_pen_size = pen_size
+                    elif mouse_pos_x > 1075  and mouse_pos_x < 1191:
+                        if math.sqrt(((mouse_pos_x % 109 - 40)**2) + ((mouse_pos_y - 762.5)**2)) < 27:
+                            pen_size = 10
+                            last_pen_size = pen_size
+                    elif mouse_pos_x > 1200:
+                        if math.sqrt(((mouse_pos_x % 109 - 40)**2) + ((mouse_pos_y - 762.5)**2)) < 27:
+                            SCREEN.fill((0, 0, 0))
+                            createGUI()
+                    
 
         elif event.type == MOUSEBUTTONUP:
             if event.button == 1:
@@ -69,8 +102,7 @@ while True:
         if left_click and mouse_pos_y < 725:
             mouse_position = pygame.mouse.get_pos()
             if last_pos is not None:
-                pygame.draw.line(SCREEN, pen_color, last_pos, mouse_position, 2)
+                pygame.draw.line(SCREEN, pen_color, last_pos, mouse_position, pen_size)
             last_pos = mouse_position
-        
 
     pygame.display.update()
--- Python/tempCodeRunnerFile.py
+++ Python/tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-
-                if math.sqrt(((mouse_pos_x % 109 - 40)**2) + ((mouse_pos_y - 762.5)**2)) < 25:
+False
