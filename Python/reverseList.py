2cb61bb739c5e792a092cd39ba610dbcc3a069ed
lbxmega <80704991+lbxmega@users.noreply.github.com>
2021-10-20 11:54:30 -0700

    adsf

--- Python/reverseList.py
+++ Python/reverseList.py
@@ -1,4 +1,4 @@
-myList = [1, 2, 3, 4, 5, "uzi", 7, "trippie", 9, 10, 11, 12, 13, 14, 15, 16, 17]
+myList = [1, 2, 3, 4, 5, "lunch", 7, "breakfast", 9, 10, 11, 12, 13, 14, 15, 16, 17]
 count = len(myList) / 2
 count = int(count)
 print(myList)
