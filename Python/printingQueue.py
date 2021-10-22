258a3184ace610f5483d14b5ccecf9e2442c8531
lbxmega <80704991+lbxmega@users.noreply.github.com>
2021-10-22 10:09:10 -0700

    this is actually pretty sick

--- Python/printingQueue.py
+++ Python/printingQueue.py
@@ -3,10 +3,14 @@ import queue
 numPages = int(input("How many pages:\n"))
 
 printQ = queue.Queue(numPages)
+f = open("Python/printingQueueFile.txt", "w")
+
 
 for x in range(numPages):
     page = input(f"Contents of the {x + 1} page:\n")
     printQ.put(page)
 
 while not printQ.empty():
-    print(printQ.get())
+    f.write(printQ.get() + "\n")
+
+f.close()
