8fa12c429b80c4c05e26c06f335ed1da7959e4bf
lbxmega <drlendog@gmail.com>
2022-02-26 20:58:20 -0800

    wer

--- Python/FlightPlan.py
+++ Python/FlightPlan.py
@@ -3,10 +3,14 @@
 # Flight Plan UI
 # Date Made: 2/26/22
 # Date Modified: 2/26/22
-
+#? preview plan
+#? create plan
+#? write plan to a file and provide file path
 from audioop import add
 import tkinter as tk
 from typing import Text
+
+debugMode = True
 
 root = tk.Tk()
 root.title("Flight Plan")
@@ -25,7 +29,14 @@ root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 #Vars
 destList = ["Start"]
 destListNums = []
+destListEntries = []
 
+
+#Classes
+class Location:
+    def __init__(self, name, pos):
+        self.name = name
+        self.pos = pos
 
 #Functions
 def temp_text(e):
@@ -34,19 +45,30 @@ def temp_text(e):
 def addDestination():
     destList.append(txt_addDest.get())
     txt_addDest.delete(0, "end")
-    print(destList)
+    if debugMode:
+        print(destList)
 
 def genList():
+    btn_getVals = tk.Button(root, text="Save Order", command=getVals)
+    btn_createPlan = tk.Button(root, text="Create Plan")
+    btn_getVals.place(x=50, y=120)
+    btn_createPlan.place(x=300, y=200)
+    destListEntries.clear()
     addY = 0
-    destListEntries = []
     for i in range(len(destList)):
         exec('tk.Label%d=tk.Label(root,text="%s")\ntk.Label%d.place(x=80, y=150+addY)' % (i,destList[i],i))
         if i > 0:
             new_entry = tk.Entry(root, width=2)
             new_entry.place(x=50, y=150+addY)
             destListEntries.append(new_entry)
-            #exec('tk.Entry%d=tk.Entry(root, text="%s", width=2)\ntk.Entry%d.place(x=50, y=150+addY)' % (i, "#", i))
-        addY += 27     
+        addY += 27   
+
+def getVals():
+    destListNums.clear()
+    for entry in destListEntries:
+        destListNums.append(entry.get())
+    if debugMode:
+        print(destListNums)
 
 #Create Widgets
 txt_addDest = tk.Entry(root, text="asdf", bd=5)
@@ -57,7 +79,6 @@ lbl_enterDes = tk.Label(root, text="    Enter Desired Destination     ")
 
 btn_addDes = tk.Button(root, text="Add", command=addDestination)
 btn_genList = tk.Button(root, text="Generate List", command=genList)
-btn_getVals
 
 #Add Widgets
 txt_addDest.place(x=40, y=50)
@@ -66,7 +87,6 @@ lbl_enterDes.place(x=40, y=30)
 
 btn_addDes.place(x=173, y=50)
 btn_genList.place(x=210, y=40)
-
 
 
 
