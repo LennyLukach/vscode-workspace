32249709ca12affd21f9d4eab5845fdd1106d91e
lbxmega <drlendog@gmail.com>
2022-05-16 23:32:46 -0700

    asdf

--- Python/General.py
+++ Python/General.py
@@ -1,43 +1,68 @@
+from asyncio.windows_events import NULL
+from cmath import nan
+from contextlib import aclosing
+import graphlib
+from cv2 import solve, sqrt
 import matplotlib.pyplot as plt
-import numpy as np; np.random.seed(1)
+import numpy as np
 
-x = np.sort(np.random.rand(15))
-y = np.sort(np.random.rand(15))
-names = np.array(list("ABCDEFGHIJKLMNO"))
+#? CONSTANTS
+X_INITIAL_VELOCITY = 5
+X_ACCELERATION = 1.2
+Y_INITIAL_VELOCITY = 0
+TIME = 4
 
-norm = plt.Normalize(1,4)
-cmap = plt.cm.RdYlGn
+#? Vars
+x_Vi = X_INITIAL_VELOCITY
+x_accel = X_ACCELERATION
+y_Vi = Y_INITIAL_VELOCITY
+y_accel = -9.8
+time = TIME
+x_distance = 0
+y_height = 0
+x_Vf = 0
+y_Vf = 0
 
-fig,ax = plt.subplots()
-line, = plt.plot(x,y, marker="o")
-
-annot = ax.annotate("", xy=(0,0), xytext=(-20,20),textcoords="offset points",
-                    bbox=dict(boxstyle="round", fc="w"),
-                    arrowprops=dict(arrowstyle="->"))
-annot.set_visible(False)
-
-def update_annot(ind):
-    x,y = line.get_data()
-    annot.xy = (x[ind["ind"][0]], y[ind["ind"][0]])
-    text = "{}, {}".format(" ".join(list(map(str,ind["ind"]))), 
-                           " ".join([names[n] for n in ind["ind"]]))
-    annot.set_text(text)
-    annot.get_bbox_patch().set_alpha(0.4)
+#? Lists
+x_graph_points = []
+y_graph_points = []
+x_dists = []
+y_heights = []
+times = []
 
 
-def hover(event):
-    vis = annot.get_visible()
-    if event.inaxes == ax:
-        cont, ind = line.contains(event)
-        if cont:
-            update_annot(ind)
-            annot.set_visible(True)
-            fig.canvas.draw_idle()
-        else:
-            if vis:
-                annot.set_visible(False)
-                fig.canvas.draw_idle()
+#? Solve for x_Vf
+def kinematic_Vf(Vi, accel, time):
+    Vf = Vi + (accel * time)
+    return Vf
 
-fig.canvas.mpl_connect("motion_notify_event", hover)
 
+#? Solve for x_distance
+def kinematic_distance(Vf, Vi, time):
+    distance = (1/2) * (Vf + Vi) * time
+    return distance
+
+
+#? Create points
+for x in range(time):
+    x_Vf = kinematic_Vf(x_Vi, x_accel, x + 1)
+    x_dist = kinematic_distance(x_Vi, x_Vf, x + 1)
+    x_graph_points.append((x_Vf, x_dist, x + 1))
+    y_Vf = kinematic_Vf(y_Vi, y_accel, x + 1)
+    y_height = kinematic_distance(y_Vi, y_Vf, x + 1)
+    y_graph_points.append((y_Vf, y_height, x + 1))
+
+#? Print organized points
+for point in range(len(x_graph_points)):
+    x_velocity = x_graph_points[point][0]
+    x_dist = x_graph_points[point][1]
+    y_height = y_graph_points[point][1]
+    time = x_graph_points[point][2]
+    x_dists.append(x_dist)
+    y_heights.append(y_height)
+    times.append(time)
+    #print(f"Velocity: {velocity} Distance: {dist} Time: {time}")
+
+plt.scatter(x_dists, y_heights)
+plt.tight_layout()
 plt.show()
--- Python/Physics Final/graphing.py
+++ Python/Physics Final/graphing.py
@@ -1,53 +1,41 @@
-from asyncio.windows_events import NULL
-from cmath import nan
-from contextlib import aclosing
-import graphlib
-from cv2 import solve, sqrt
+from dis import dis
+import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-#? CONSTANTS
-INITIAL_VELOCITY = 4
-TIME = 14
-ACCELERATION = 1.23
+sind = lambda degrees: round(np.sin(np.deg2rad(degrees)), 3)
+cosd = lambda degrees: round(np.cos(np.deg2rad(degrees)), 3)
 
+distances = []
+heights = []
 
-#? Vars
-Vi = INITIAL_VELOCITY
-time = TIME
-accel = ACCELERATION
-distance = 0
-Vf = 0
+launch_angle = 30
+Vi = 10
+accely = -9.8
 
-#? Lists
-graph_points = []
+Viy = Vi * sind(launch_angle)
+Vfy = -Viy
+Vix = Vi * cosd(launch_angle)
+delta_V = Vfy - Viy
 
+time = round(delta_V/accely, 2)
+time_increment = round(time/10, 2)
 
-#? Solve for time
-def kinematic_Vf(Vi, accel, time):
-    Vf = Vi + (accel * time)
-    return Vf
-
-
-#? Solve for distance
-def kinematic_distance(Vf, Vi, time):
-    distance = (1/2) * (Vf + Vi) * time
-    return distance
-
-
-#? Create points
-for x in range(time):
-    finalV = kinematic_Vf(Vi, accel, x + 1)
-    dist = kinematic_distance(Vi, finalV, x + 1)
-    graph_points.append((finalV, dist, x + 1))
-
-
-
-#? Print organized points
 '''
-for point in range(len(graph_points)):
-    velocity = graph_points[point][0]
-    dist = graph_points[point][1]
-    time = graph_points[point][2]
-    print(f"Velocity: {velocity} Distance: {dist} Time: {time}")
+Viy = Vi * sind(launch_angle)
+Vfy = -Viy
+accely = -9.8
+delta_V = Vfy - Viy
+time = round(delta_V/accely, 2)
+
+Vix = Vi * cosd(launch_angle)
+Sx = round(Vix * time, 2)
 '''
+
+for x in range(10):
+    Sx = round(Vix * time, 2)
+    Sy = round(Viy * time, 2)
+    distances.append(Sx)
+    heights.append(Sy)
+
+print(distances)
