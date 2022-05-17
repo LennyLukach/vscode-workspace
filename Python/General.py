3b94de6fe06c2680448d930c0205459b4601d699
lbxmega <drlendog@gmail.com>
2022-05-17 01:10:09 -0700

    asdf

--- Python/General.py
+++ Python/General.py
@@ -1,68 +1,12 @@
-from asyncio.windows_events import NULL
-from cmath import nan
-from contextlib import aclosing
-import graphlib
-from cv2 import solve, sqrt
 import matplotlib.pyplot as plt
-import numpy as np
 
-#? CONSTANTS
-X_INITIAL_VELOCITY = 5
-X_ACCELERATION = 1.2
-Y_INITIAL_VELOCITY = 0
-TIME = 4
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
 
-#? Vars
-x_Vi = X_INITIAL_VELOCITY
-x_accel = X_ACCELERATION
-y_Vi = Y_INITIAL_VELOCITY
-y_accel = -9.8
-time = TIME
-x_distance = 0
-y_height = 0
-x_Vf = 0
-y_Vf = 0
+x = [0, 1, 2, 3]
+y = [3, 2, 1, 0]
 
-#? Lists
-x_graph_points = []
-y_graph_points = []
-x_dists = []
-y_heights = []
-times = []
+points = ax.scatter(x, y)
+print(points.get_offsets().data)
 
-
-#? Solve for x_Vf
-def kinematic_Vf(Vi, accel, time):
-    Vf = Vi + (accel * time)
-    return Vf
-
-
-#? Solve for x_distance
-def kinematic_distance(Vf, Vi, time):
-    distance = (1/2) * (Vf + Vi) * time
-    return distance
-
-
-#? Create points
-for x in range(time):
-    x_Vf = kinematic_Vf(x_Vi, x_accel, x + 1)
-    x_dist = kinematic_distance(x_Vi, x_Vf, x + 1)
-    x_graph_points.append((x_Vf, x_dist, x + 1))
-    y_Vf = kinematic_Vf(y_Vi, y_accel, x + 1)
-    y_height = kinematic_distance(y_Vi, y_Vf, x + 1)
-    y_graph_points.append((y_Vf, y_height, x + 1))
-
-#? Print organized points
-for point in range(len(x_graph_points)):
-    x_velocity = x_graph_points[point][0]
-    x_dist = x_graph_points[point][1]
-    y_height = y_graph_points[point][1]
-    time = x_graph_points[point][2]
-    x_dists.append(x_dist)
-    y_heights.append(y_height)
-    times.append(time)
-    #print(f"Velocity: {velocity} Distance: {dist} Time: {time}")
-
-plt.scatter(x_dists, y_heights)
-plt.tight_layout()
 plt.show()
--- Python/Physics Final/graphing.py
+++ Python/Physics Final/graphing.py
@@ -8,9 +8,12 @@ cosd = lambda degrees: round(np.cos(np.deg2rad(degrees)), 3)
 
 distances = []
 heights = []
+pointSpots = []
+speedX = []
+speedY = []
 
-launch_angle = 30
-Vi = 10
+launch_angle = 34
+Vi = 3.89
 accely = -9.8
 
 Viy = Vi * sind(launch_angle)
@@ -19,7 +22,7 @@ Vix = Vi * cosd(launch_angle)
 delta_V = Vfy - Viy
 
 time = round(delta_V/accely, 2)
-time_increment = round(time/10, 2)
+time_increment = time/10
 
 '''
 Viy = Vi * sind(launch_angle)
@@ -32,10 +35,31 @@ Vix = Vi * cosd(launch_angle)
 Sx = round(Vix * time, 2)
 '''
 
-for x in range(10):
-    Sx = round(Vix * time, 2)
-    Sy = round(Viy * time, 2)
+for x in range(11):
+    Sx = round(Vix * time_increment * (x), 2)
+    Sy = round(Viy * time_increment * (x) + (1/2) * accely * pow((time_increment * (x)), 2) , 2)
     distances.append(Sx)
     heights.append(Sy)
+    speedX.append(Vix)
+    speedY.append(Viy)
 
 print(distances)
+print(heights)
+
+fig, ax = plt.subplots()
+
+points = ax.scatter(distances, heights)
+ax.set_title("Projectile Trajectory Graph")
+ax.set_xlabel("Distance")
+ax.set_ylabel("Height")
+
+for point in range(11):
+    pointSpots.append((points.get_offsets().data[point][0], points.get_offsets().data[point][1]))
+
+for point in pointSpots:
+    plt.annotate(f"Vx:{1}m/s\nVy:{2}m/s\nX:{point[0]}\nY:{point[1]}", point, horizontalalignment='left', verticalalignment='center') 
+
+print(speedX)
+print(speedY)
+
+plt.show()
