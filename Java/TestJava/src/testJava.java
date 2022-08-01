b6ce12ed86e4a57e1cd7d39a7ef7c9fc6502b04e
lbxmega <drlendog@gmail.com>
2022-08-01 15:11:57 -0700

    sdf

--- Java/TestJava/src/testJava.java
+++ Java/TestJava/src/testJava.java
@@ -1,5 +1,8 @@
 public class testJava {
     public static void main(String[] args) {
         
+
+        var = (System.getProperty("user.home") + "/AppData/Local/Google/Chrome/User Data/");
+
     }
 }
