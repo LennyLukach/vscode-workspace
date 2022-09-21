95851d11ab842eaaec9fa7112c5b8424f473d452
lbxmega <drlendog@gmail.com>
2022-09-20 23:20:08 -0700

    asdf

---
+++ C#/TestConsoleApp/TextX.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            for(int x = 1; x <= 10; x++)
+            {
+                Console.WriteLine("X: " + x);
+            }
+        }
+    }
+}
