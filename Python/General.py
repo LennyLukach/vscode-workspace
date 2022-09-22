469ae9675d8acac22a81c3efaf60e31468ee2575
lbxmega <80704991+lbxmega@users.noreply.github.com>
2022-09-22 10:37:10 -0700

    jb

--- Python/General.py
+++ Python/General.py
@@ -1,9 +1,2 @@
-<<<<<<< HEAD
 import math
 print(round(math.sqrt(10), 1))
-=======
-from turtle import *
-space = Screen()
-alex = Turtle()
-alex.forward(100)
->>>>>>> 95851d11ab842eaaec9fa7112c5b8424f473d452
