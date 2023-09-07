e99814122f7c0c12701aa8c4e0a2c063aa991014
lbxmega <drlendog@gmail.com>
2023-09-06 21:59:47 -0700

    asdf

--- Java/TestJava/src/testJava.java
+++ Java/TestJava/src/testJava.java
@@ -1,5 +1,34 @@
 public class testJava {
+
     public static void main(String[] args) {
 
+        Account a = new Account("Lenny", 17);
+        a.accInfo();
+        try {
+            a.name = "s";
+        }
+        catch (Exception e) {
+            a.setName("t");
+        }
+       
+    }
+
+}
+
+class Account {
+    private String name;
+    private int age;
+
+    Account(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public void accInfo() {
+        System.out.println(this.name + "\n" + this.age);
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
