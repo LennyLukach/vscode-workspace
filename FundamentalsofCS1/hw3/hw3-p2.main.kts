38a88348cc440b2d3029d37c99139309261782ee
lbxmega <80704991+lbxmega@users.noreply.github.com>
2023-09-22 12:45:51 -0700

    asdf

---
+++ FundamentalsofCS1/hw3/hw3-p2.main.kts
@@ -0,0 +1,42 @@
+// -----------------------------------------------------------------
+// Homework 3, Problem 2
+// -----------------------------------------------------------------
+
+// TODO 1/4: Design the data type FlashCard to represent a single
+//           flash card. You should be able to represent the text
+//           prompt on the front of the card as well as the text
+//           answer on the back. Include at least 3 example cards
+//           (which will come in handy later for tests!).
+//
+
+// TODO 2/4: Design the data type Deck to represent a deck of
+//           flash cards. The deck should have a name, as well
+//           as a sequence of flash cards.
+//
+//           Include at least 2 example decks based upon the
+//           card examples above.
+//
+
+// TODO 3/4: Design the predicate areAllOneWordAnswers that
+//           determines if the backs of all the cards in a deck
+//           are a single word (i.e., have no spaces, which
+//           includes a card with a blank back).
+//
+//           Hint: hidden in the name of this function is a
+//                 reminder of a useful list function to use :)
+//
+
+// A couple potentially helpful examples for tests
+// val fcEmptyBack = FlashCard("Front", "")
+// val fcLongBack = FlashCard("Front", "Long answer")
+
+// TODO 4/4: Design the predicate anyContainsPhrase that determines
+//           if any of the cards in a deck contain the supplied
+//           phrase.
+//
+//           Hints:
+//           - string1.contains(string2) will be quite useful
+//             here :)
+//           - Again, the name of this function hints at a useful
+//             list function we learned!
+//
---
+++ FundamentalsofCS1/hw3/hw3-p3.main.kts
@@ -0,0 +1,97 @@
+// -----------------------------------------------------------------
+// Homework 3, Problem 3
+// -----------------------------------------------------------------
+
+// TODO 1/1: Design the program showNumbers that uses reactConsole
+//           to simply print to the screen all the numbers in a
+//           supplied list. For example, running...
+//
+//           showNumbers(listOf(5, 4, 3, 2, 1))
+//
+//           should show at the terminal...
+//
+//
+//           5
+//
+//           4
+//
+//           3
+//
+//           2
+//
+//           1
+//
+//           Done!
+//
+//
+//           (Note: this assumes the user doesn't type anything
+//           between the numbers; even if they did, you should
+//           ignore it in the transition function.)
+//
+//           Remember to start by thinking about your state
+//           representation. While there are multiple approaches,
+//           here are a couple suggestions:
+//
+//           1. Represent state as a list; transitioning to the
+//              next list then involves list.drop(1) to produce
+//              a new list without the first element of the old
+//              list; you'll want to check for list.isEmpty()
+//
+//           2. Design a data class that keeps the supplied list
+//              AND the current index (that you can increment
+//              until it is no longer valid)
+//
+//           To help, you have a couple tests, which don't at all
+//           depend on how *you* choose to represent state :)
+//
+//           If you need a refresher on reactConsole, recall that
+//           there are a series of videos/files on Canvas walking
+//           you through the design process and some example
+//           programs :)
+//
+
+// @EnabledTest
+// fun testShowNumbers() {
+//     // makes a captureResults-friendly function :)
+//     fun helpTest(numList: List<Int>): () -> Unit {
+//         fun showMyNumbers() {
+//             showNumbers(numList)
+//         }
+
+//         return ::showMyNumbers
+//     }
+
+//     testSame(
+//         captureResults(
+//             helpTest(emptyList<Int>()),
+//             "",
+//         ),
+//         CapturedResult(
+//             Unit,
+//             "Done!",
+//         ),
+//         "empty",
+//     )
+
+//     testSame(
+//         captureResults(
+//             helpTest(listOf(5, 4, 3, 2, 1)),
+//             "",
+//             "",
+//             "",
+//             "",
+//             "",
+//             "",
+//         ),
+//         CapturedResult(
+//             Unit,
+//             "5",
+//             "4",
+//             "3",
+//             "2",
+//             "1",
+//             "Done!",
+//         ),
+//         "5/4/3/2/1",
+//     )
+// }
