fd4e86727b78740fd210cdf0ff119325c4dbd244
lennylukach <drlendog@gmail.com>
2023-09-29 11:31:28 -0700

    asdf

--- Python/cartesianMath.py
+++ Python/cartesianMath.py
@@ -1,13 +1,9 @@
-# write function to do cartesian multiplication
 
 import numpy as np
 
+
 def cartesianMath(x, y, op):
-    # x, y are 2D arrays
-    # op is a string, either 'add' or 'mult'
-    # returns a 3D array of shape (x.shape[0], y.shape[0], x.shape[1])
-    # where the last dimension is the result of the operation
-    # on the corresponding elements of x and y
+
     if op == 'add':
         return np.add.outer(x, y)
     elif op == 'mult':
@@ -16,4 +12,4 @@ def cartesianMath(x, y, op):
         raise ValueError('op must be either add or mult')
     
 
-cartesianMath(np.array([1,2,3]), np.array([4,5,6]), 'add')
+print(cartesianMath(np.array([1,2,3]), np.array([4,5,6]), 'mult'))
