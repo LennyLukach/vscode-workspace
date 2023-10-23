21069352560a1bdd651f2ec2c77b0b416639ba93
lennylukach <drlendog@gmail.com>
2023-10-23 14:04:22 -0700

    asdf

--- Java/TestJava/src/testJava.java
+++ Java/TestJava/src/testJava.java
@@ -1,21 +1,5 @@
-class Account {
-    private String name;
-    private int age;
-
-    Account(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-
-    public void accInfo() {
-        System.out.println(this.name + "\n" + this.age);
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
+public class testJava {
+    public static void main(String[] args) {
+        System.out.println("kys");
     }
 }
