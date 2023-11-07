6aa315dabd88ba379e7e9484a9cc436ef5c1a4a9
lennylukach <drlendog@gmail.com>
2023-11-07 13:15:13 -0800

    asdf

---
+++ FundamentalsofCS1/hw6/hw6-p1.main.kts
@@ -0,0 +1,22 @@
+// -----------------------------------------------------------------
+// Homework 6, Problem 1
+// -----------------------------------------------------------------
+
+// In this problem, you'll practice with looping and mutation to
+// implement a form of the functional `map` function to actually
+// change the elements of a supplied list (instead of returning
+// a new list).
+
+// TODO 1/1: Design the function myInplaceMap, which transforms the
+//           elements of a supplied list using a supplied function.
+//
+//           For example, if the function was supplied [1, 2, 3] as
+//           a mutable list, as well as a function that doubles a
+//           supplied number, the supplied list would be [2, 4, 6]
+//           after the function completed (note that the function
+//           itself should not return any value).
+//
+//           Make sure to sufficiently test this function,
+//           including empty vs lists that have elements, as well
+//           as differing types.
+//
